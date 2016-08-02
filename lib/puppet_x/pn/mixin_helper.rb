2b41215996e1c45048a3c519c8643dabbec13abf
Kyle <kylechickering@yahoo.com>
2016-08-02 16:28:28 -0700

    Remove legacy method
    Removed the deconstruct_range method because it is no longer used by the Puppet module

--- lib/puppet_x/pn/mixin_helper.rb
+++ lib/puppet_x/pn/mixin_helper.rb
@@ -10,27 +10,6 @@ module PuppetX
       # PDQ means no 'no-show-headers' is needed
       PDQ = ['parsable-delim', '%', Q]
 
-      # Feed a range, returns an array.
-      # accepts comma separated, with or without whitespace, and range operators
-      # if the numbers are reversed it will add them in order as if they were in
-      # the correct order.
-      # all valid ranges: '1', '1,2,3', '1-3', '1-3, 4-7,8-19,    32-22, 88-88'
-      def deconstruct_range (range)
-        out = []
-        return out if range.length == 0
-        range.to_s.gsub(' ', '').split(',').each do |s|
-          if s =~ /-/
-            lo, hi = s.split('-')
-            hi = hi.to_i; lo = lo.to_i
-            hi, lo = lo, hi if hi < lo
-            hi == lo ? out.push(hi.to_s) : (lo..hi).each { |i| out.push i.to_s }
-          else
-            out.push s.to_s
-          end
-        end
-        out
-      end
-
       # Sometimes you need a SPLAT!
       # This method does the same thing as current switch but the return value
       # from this method can be splatted into commands that use shell ordering
@@ -51,17 +30,15 @@ module PuppetX
       #     'eth#.vlan#' instead of 'eth#.'.
       # @param vrouter_name: The name of the vrouter where the nic is being
       #     investigated. Defaults to the vrouter specified in the manifest.
-      # @param vlan: The associated vlan to the desired nic. Defaults to the
-      #     vlan specified in the manifest file.
       # @return: A string containing the nic.
       #
       def get_nic(include_vlan=0, vrouter_name="#{resource[:vrouter]}",
-                  ipin="#{resource[:ip]}", mask='24', vlan='101')
+                  ipin="#{resource[:ip]}", mask='24')
         out = cli(*splat_switch, "vrouter-interface-show", "vrouter-name",
                "#{vrouter_name}", "format", "nic,ip", PDQ)
         out.split("\n").each do |interface|
           vrouter, nic, ip = interface.split('%')
-          if ip.strip == build_ip(0, ipin, mask, vlan)
+          if ip.strip == build_ip(0, ipin, mask)
             include_vlan != 0 ? (return nic) : (return nic.split('.')[0] + '.')
           end
         end
@@ -82,18 +59,12 @@ module PuppetX
       # @param mask: The netmask to be appended to the ip, do not include a '/',
       #     just the actual number of the netmask. Netmask appending can be
       #     toggled with the 'nomask' parameter.
-      # @param vlan: The vlan id. This value will replace any instance of 'x' in
-      #     the submitted ip parameter. This parameter's default value is 0.
       # @return: A string containing the generated ipv4 ip.
       #
-      def build_ip(nomask=0, ip="#{@resource[:ip]}", mask="#{@resource[:mask]}",
-                   vlan="#{@resource[:vlan]}")
+      def build_ip(nomask=0, ip="#{@resource[:ip]}", mask="#{@resource[:mask]}")
         k = ip.split('.')
         ip_out = ''
         for i in (0..3)
-          if k[i] == 'x'
-            k[i] = "#{vlan}"
-          end
           if i == 3
             ip_out += "#{k[i]}"
           else
